6d5ad3af356d28a5fce0b5a2c4f79c4cdff8b501
pzawi <p.zawidzki665@gmail.com>
2023-04-23 22:57:54 +0200

    clear console

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Movie from "./Movie"
 export default  async function Home() {
   const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`, {next: {revalidate: 60 }}) 
   const res = await data.json()
-  console.log(res)
+  
   return (
     <main>
       <div className="grid gap-16 grid-cols-fluid">
